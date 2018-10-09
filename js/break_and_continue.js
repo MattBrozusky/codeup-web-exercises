4f521936ced21624c0ddf9dbfc08d6dd04fee6df
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-09 12:13:29 -0500

    finished loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -10,3 +10,79 @@
 
 
 
+
+
+
+
+//
+// for (var x = 2; x % 2 === 0;) {
+//
+//     x = prompt("Please give an odd number between 1 and 50:");
+//
+//     if (x <= 0) {
+//         alert("Invalid input");
+//         x = 0;
+//         continue;
+//     }
+//     if (x >= 51) {
+//         alert("Invalid input");
+//         x = 0;
+//         continue;
+//     }
+//     if (x % 2 === 0) {
+//         alert("That's an even number!")
+//     }
+//
+// }
+
+
+
+
+
+
+
+
+// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+
+
+
+
+
+
+// var y = prompt("Please give an odd number between 1 and 50:");
+//
+//
+// for (var x = 1; x <= 50; x += 2) {
+//
+//
+//     if (x === parseFloat(y)) {
+//         console.log("Number skipped is " + y);
+//         continue;
+//     } else if (y % 2 === 0) {
+//         while (y % 2 === 0) {
+//             alert("Enter a valid input!");
+//             y = prompt("Please give an odd number between 1 and 50:");
+//
+//         }
+//
+//     }
+//     console.log("Here is an odd number: " + x);
+//
+//
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

b1d5b39c05b5b715a79b9e0c7e58da70c679412b
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-21 21:25:45 -0500

    Added some html outline pages to MMA project

--- Personal-Projects/mixed-methods-art/js/mix-methods-art.js
+++ Personal-Projects/mixed-methods-art/js/mix-methods-art.js
@@ -1,17 +1,25 @@
 "use strict";
-(function () {
-
 
 $(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
@@ -40,5 +48,3 @@ $(document).ready(function () {
 
 
 
-
-})();

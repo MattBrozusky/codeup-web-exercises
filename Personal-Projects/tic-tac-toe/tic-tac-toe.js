b22cb7bb218c8206ac05bf3d32d0fcd948f90409
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-22 14:59:09 -0500

    started tic-tac-toe bonus

---
+++ Personal-Projects/tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,29 @@
+"use strict";
+$(document).ready(function () {
+
+
+    $(".box").click(function () {
+
+        $(this).append("<span class='letter-style'>X</span>");
+        console.log($(this).html());
+        if ($(this).html()) {
+
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

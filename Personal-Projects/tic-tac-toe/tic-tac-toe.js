80ce5c30e27060ec4d51a94e7e054593bdf2717e
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-23 11:13:43 -0500

    Minor changes

--- Personal-Projects/tic-tac-toe/tic-tac-toe.js
+++ Personal-Projects/tic-tac-toe/tic-tac-toe.js
@@ -2,14 +2,36 @@
 $(document).ready(function () {
 
 
-    $(".box").click(function () {
 
-        $(this).append("<span class='letter-style'>X</span>");
-        console.log($(this).html());
-        if ($(this).html()) {
+
+
+
+    var move = 1;
+    var play = true;
+
+    $(".box").on("click", function () {
+        if (move % 2 !== 2 % 2 && ($(this).html() !== "X" || $(this).html() !== "Y")) {
+
+
+                $(this, "span").append("X");
+                if ($(this).html("X") || $(this).html("Y")) {
+                    $('.this').off('click');
+                }
+                move += 1;
+
+        } else if (move % 2 === 2 % 2 && ($(this).html() !== "X" || $(this).html() !== "Y")) {
+
+
+                $(this, "span").append("Y");
+                if ($(this).html("Y") || $(this).html("X")) {
+                    $('.this').off('click');
+                }
+                move += 1;
+
 
         }
-    })
+
+    });
 
 
 

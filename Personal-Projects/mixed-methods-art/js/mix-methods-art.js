ba5307a22d08b796e427193c110bcffc160ece23
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-23 15:58:58 -0500

    Made progress with js updated html elements for paintings

--- Personal-Projects/mixed-methods-art/js/mix-methods-art.js
+++ Personal-Projects/mixed-methods-art/js/mix-methods-art.js
@@ -2,13 +2,11 @@
 
 $(document).ready(function () {
 
-
-
-
+//-----------------
+// Carousel JS//
+//-----------------
 
     $('#carouselExample').on('slide.bs.carousel', function (e) {
-
-
         var $e = $(e.relatedTarget);
         var idx = $e.index();
         var itemsPerSlide = 4;
@@ -28,11 +26,9 @@ $(document).ready(function () {
         }
     });
 
-
     $('#carouselExample').carousel({
-        interval: 2000
+        interval: 5000
     });
-
 
     $(document).ready(function() {
         /* show lightbox when clicking a thumbnail */
@@ -49,14 +45,121 @@ $(document).ready(function () {
     });
 
 
+//-----------------
+// Paintings Card Creations JS//
+//-----------------
+
+    var paintings = [{
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 1,
+            name: 'Light City',
+            description: 'light'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 2,
+            name: 'Half City',
+            description: 'light'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 3,
+            name: 'Cinnamon',
+            description: 'light'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 4,
+            name: 'City',
+            description: 'medium'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 5,
+            name: 'American',
+            description: 'medium'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 6,
+            name: 'Breakfast',
+            description: 'medium'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 7,
+            name: 'High',
+            description: 'dark'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 8,
+            name: 'Continental',
+            description: 'dark'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 9,
+            name: 'New Orleans',
+            description: 'dark'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 10,
+            name: 'European',
+            description: 'dark'
+        },
+        {
+            price: 11,
+            name: 'Espresso',
+            description: 'dark'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 12,
+            name: 'Viennese',
+            description: 'dark'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 13,
+            name: 'Italian',
+            description: 'dark'
+        },
+        {
+            imgFile: '../img/mma-placeholder.jpg',
+            price: 14,
+            name: 'French',
+            description: 'dark'
+        }
+    ];
+
+    function renderPainting(painting) {
+        var html = '<article class="col-12 col-md-6 col-xl-4 mt-5">';
+        html += '<div class="card">';
+        html += '<h4 class="card-title text-center mt-2">' + painting.name + '</h4>';
+        html += '<img class="card-img-top" src="' + painting.imgFile + '" alt="Card image cap">';
+        html += '<div class="card-body">';
+        html += '<p class="card-text">' + painting.description + '</p>';
+        html += '</div>';
+        html += '<h6 class="card-subtitle mb-2 mr-4 text-muted text-right">' + painting.price  + '</h6>';
+        html += '</div>';
+        html += '</article>';
+        return html;
+    }
 
 
 
+    function renderPaintings(paintings) {
+        var html = '';
+        for(var i = 0; i < paintings.length; i++) {
+            html += renderPainting(paintings[i]);
+        }
+        return html;
+    }
 
 
-
-
-
+    $("#img-cards").append(renderPaintings(paintings));
 
 
 

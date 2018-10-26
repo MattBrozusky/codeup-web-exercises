a4982287a58c0dc47382756f9d07cdafe41f1b31
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-26 16:38:10 -0500

    Made a bunch of progress on weather map

--- js/weather-map.js
+++ js/weather-map.js
@@ -2,32 +2,107 @@ $(document).ready(function () {
     "use strict";
 
 
+    const getMinMaxDayTemp = (data, day) => {
+        const temps = data.list.slice(day * 8 - 8, day * 8)
+            .reduce((prev, curr) => {
+                prev.push(curr.main.temp);
+                return prev;
+            }, []).sort();
+        return {min: temps.shift(), max: temps.pop()};
+    };
+
+
     function weatherData() {
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             APPID: "d8078a58900152b022c837716684ad65",
-            q:     "San Antonio, US"
+            q:     "San Antonio, US",
+            units: "imperial"
         }).done(function (data) {
             console.log(data);
+            $("#weather-cards").html(createCards(data));
         });
     }
-
     weatherData();
 
 
+    function createCards(data) {
+        var html = '';
+        for (var i = 0; i < 3; i++) {
+            var temp = getMinMaxDayTemp(data, i + 1);
+
+            html += '<article class="col s4">';
+            html += '<div class="card blue-grey darken-1">';
+            html += '<div class="card-content white-text">';
+            html += '<span class="card-title center">' + temp.min.toFixed(0) + ' / ' + temp.max.toFixed(0) + '</span>';
+            html += '<div class="center"><img src="http://openweathermap.org/img/w/' + data.list[i * 8].weather[0].icon + '.png" alt=""></div>';
+            html += '<hr>';
+            html += '<h6><span class="weather-header">CLOUDS:</span>' + ' ' + data.list[i * 8].weather[0].description + '</h6>';
+            html += '<h6><span class="weather-header">HUMIDITY:</span>' + ' ' + data.list[i * 8].main.humidity + '</h6>';
+            html += '<h6><span class="weather-header">WIND:</span>' + ' ' + data.list[i * 8].wind.speed + '</h6>';
+            html += '<h6><span class="weather-header">PRESSURE:</span>' + ' ' + data.list[i * 8].main.pressure + '</h6>';
+            html += '</div>';
+            html += '</div>';
+            html += '</article>';
+        }
+        return html;
+    }
 
 
+
+    //------------------
+    // Google Maps
+    //------------------
+
+    var mapOptions ={
+        center: {
+            lat: 29.397,
+            lng: -98.5
+        },
+        zoom: 12
+    };
+
+    var geocoder = new google.maps.Geocoder();
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var address = prompt("Enter an address you want to find.");
+
+
+
+
+    geocoder.geocode({ "address": address }, function(results, status) {
+
+        function toggleBounce() {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        }
+
+        if (status === google.maps.GeocoderStatus.OK) {
+
+            // Recenter the map over the address
+            map.setCenter(results[0].geometry.location);
+
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map,
+                draggable: true,
+                animation: google.maps.Animation.DROP
+            });
+
+            marker.addListener("click", toggleBounce);
+        } else {
+
+
+            alert("Geocoding was not successful - STATUS: " + status);
+        }
+    });
 // MAKE THE WEATHER FORECAST
-    // practice writing console.logs of various parts of the OpenWeatherMap object...
-    // console.log the current weather description
-    // console.log the humidity of the 2nd day
-    // console.log the humidity of all the days (use a for loop)
-    // console.log the temperature of the 3rd day in celsius
-    // console.log the temperature of the 3rd day in Fahrenheit
-    // console.log all types of information you will ultimately display for a single day in the 3-day forecast div
-    // console.log all type of information you will ultimately display for all days in the 3-day forecast divs
-    // write html and css to make/style a three-day forecast set of divs (may use design from curriculum)
+
     // create a function to append specific parts of the OpenWeatherMap map object to the DOM and call it in the .done() of the OpenWeatherMap map GET request.
-    // remember to first console.log specific data from the OpenWeatherMap object, then work on adding to the DOM
+    
     // read and reread the curriculum and OpenWeatherMap docs carefully on how to get an image of the weather forecast
     // hint: "http://openweathermap.org/img/w/" + whateverTheWeatherIconValueIs
     // verify that the OpenWeatherMap data populates by hard coding in lat and lon values

2b3a8d836dffe25771ed1453131fe648bb00f230
MattBrozusky <matthew.brozusky@gmail.com>
2018-10-30 09:40:55 -0500

    more weather map updates

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
     "use strict";
 
+    var mapPosition = {
+        lat: "29.4241",
+        lon: "-98.4936"
+
+    };
+
     function weatherData() {
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
-            APPID: "",
-            lat: "29.4241",
-            lon: "-98.4936",
+            APPID: "d8078a58900152b022c837716684ad65",
+            lat: mapPosition.lat,
+            lon: mapPosition.lon,
             units: "imperial"
         }).done(function (data) {
             // console.log(data);
+            $("#marker-location").html(data.city.name);
             $("#weather-cards").html(createCards(data));
         });
     }
@@ -24,33 +31,36 @@ $(document).ready(function () {
     };
 
 
-
-
     function createCards(data) {
         var today = new Date();
         var html = '';
+
+        // console.log(today);
+        // console.log(typeof(today));
 
         for (var i = 0; i < 3; i++) {
             var day = new Date(today.getFullYear(), today.getMonth(), today.getDate() + i);
             day = day.toString();
             var temp = getMinMaxDayTemp(data, i + 1);
 
-            html += '<article class="col s12">';
-            html += '<div class="card blue-grey darken-1 z-depth-1 hoverable">';
-            html += '<div class="card-content white-text">';
+            html += '<article class="col s12 extra-content">';
+            html += '<div class="card horizontal blue-grey darken-1 z-depth-1 hoverable border-radius-round">';
+            html += '<div class="card-stacked">';
+            html += '<section class="card-content white-text">';
             html += '<span class="card-title center">' + day.substring(0, 10) + '</span>';
             html += '<hr>';
-            html += '<section class="row mb-0">';
-            html += '<article class="col s6">';
+            html += '<article class="row mb-0">';
+            html += '<div class="center card-title">' + temp.min.toFixed(0) + ' / ' + temp.max.toFixed(0) + '</div>';
+            html += '<div class="center"><img src="http://openweathermap.org/img/w/' + data.list[i * 8].weather[0].icon + '.png" alt=""></div>';
+            html += '</article>';
+            html += '</section>';
+            html += '</div>';
+            html += '<div class="card-stacked hide extra-slide">';
+            html += '<section class="card-content white-text">';
             html += '<h6><span class="weather-header">CLOUDS:</span>' + ' ' + data.list[i * 8].weather[0].description + '</h6>';
             html += '<h6><span class="weather-header">HUMIDITY:</span>' + ' ' + data.list[i * 8].main.humidity + '</h6>';
             html += '<h6><span class="weather-header">WIND:</span>' + ' ' + data.list[i * 8].wind.speed + '</h6>';
             html += '<h6><span class="weather-header">PRESSURE:</span>' + ' ' + data.list[i * 8].main.pressure + '</h6>';
-            html += '</article>';
-            html += '<article class="col s6 mt-4">';
-            html += '<div class="center card-title">' + temp.min.toFixed(0) + ' / ' + temp.max.toFixed(0) + '</div>';
-            html += '<div class="center"><img src="http://openweathermap.org/img/w/' + data.list[i * 8].weather[0].icon + '.png" alt=""></div>';
-            html += '</article>';
             html += '</section>';
             html += '</div>';
             html += '</div>';
@@ -60,7 +70,13 @@ $(document).ready(function () {
     }
 
 
-
+    $(document).on('mouseenter', '.extra-content', function () {
+        $(this).children().children().next().fadeIn(750).removeClass("hide");
+    });
+    $(document).on('mouseleave', '.extra-content', function () {
+        $(this).children().children().next().fadeOut(750);
+    });
+    
     //------------------
     // Google Maps
     //------------------
@@ -103,19 +119,10 @@ $(document).ready(function () {
             }
 
             marker.addListener('dragend', function (event) {
-                var lat = this.getPosition().lat();
-                var lon = this.getPosition().lng();
+                mapPosition.lat = this.getPosition().lat();
+                mapPosition.lon = this.getPosition().lng();
 
-                $.get("http://api.openweathermap.org/data/2.5/forecast", {
-                    APPID: "",
-                    lat: lat,
-                    lon: lon,
-                    units: "imperial"
-                }).done(function (data) {
-                    console.log(data);
-                    $("#marker-location").html(data.city.name);
-                    $("#weather-cards").html(createCards(data));
-                });
+                weatherData()
             });
         });
     }
